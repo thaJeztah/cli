64a4ba309848fcfa7c389d59938524a86107ef35
Sebastiaan van Stijn <github@gone.nl>
2025-02-03 20:20:07 +0100

    cli/connhelper/ssh: TestParseURL: minor cleaning up
    - use designated example domains as example value
    - swap "expected" and "actual" values in assertions
    - also test the Spec that's produced, not just the args
    - merge two test-cases that could be combined
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/connhelper/ssh/ssh_test.go
+++ cli/connhelper/ssh/ssh_test.go
@@ -12,37 +12,54 @@ func TestParseURL(t *testing.T) {
 		url           string
 		expectedArgs  []string
 		expectedError string
+		expectedSpec  Spec
 	}{
 		{
-			url: "ssh://foo",
+			url: "ssh://example.com",
 			expectedArgs: []string{
-				"--", "foo",
+				"--", "example.com",
+			},
+			expectedSpec: Spec{
+				Host: "example.com",
 			},
 		},
 		{
-			url: "ssh://me@foo:10022",
+			url: "ssh://me@example.com:10022",
 			expectedArgs: []string{
 				"-l", "me",
 				"-p", "10022",
-				"--", "foo",
+				"--", "example.com",
+			},
+			expectedSpec: Spec{
+				User: "me",
+				Host: "example.com",
+				Port: "10022",
 			},
 		},
 		{
-			url:           "ssh://me:passw0rd@foo",
+			url: "ssh://me@example.com:10022/var/run/docker.sock",
+			expectedArgs: []string{
+				"-l", "me",
+				"-p", "10022",
+				"--", "example.com",
+			},
+			expectedSpec: Spec{
+				User: "me",
+				Host: "example.com",
+				Port: "10022",
+				Path: "/var/run/docker.sock",
+			},
+		},
+		{
+			url:           "ssh://me:passw0rd@example.com",
 			expectedError: "plain-text password is not supported",
 		},
 		{
-			url: "ssh://foo/bar",
-			expectedArgs: []string{
-				"--", "foo",
-			},
-		},
-		{
-			url:           "ssh://foo?bar",
+			url:           "ssh://example.com?bar",
 			expectedError: `extra query after the host: "bar"`,
 		},
 		{
-			url:           "ssh://foo#bar",
+			url:           "ssh://example.com#bar",
 			expectedError: `extra fragment after the host: "bar"`,
 		},
 		{
@@ -50,7 +67,7 @@ func TestParseURL(t *testing.T) {
 			expectedError: "no host specified",
 		},
 		{
-			url:           "foo://bar",
+			url:           "foo://example.com",
 			expectedError: `expected scheme ssh, got "foo"`,
 		},
 	}
@@ -59,9 +76,11 @@ func TestParseURL(t *testing.T) {
 			sp, err := ParseURL(tc.url)
 			if tc.expectedError == "" {
 				assert.NilError(t, err)
-				assert.Check(t, is.DeepEqual(tc.expectedArgs, sp.Args()))
+				assert.Check(t, is.DeepEqual(sp.Args(), tc.expectedArgs))
+				assert.Check(t, is.Equal(*sp, tc.expectedSpec))
 			} else {
-				assert.ErrorContains(t, err, tc.expectedError)
+				assert.Check(t, is.Error(err, tc.expectedError))
+				assert.Check(t, is.Nil(sp))
 			}
 		})
 	}
